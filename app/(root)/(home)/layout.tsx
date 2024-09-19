c0539cbc5e06163c26edf20f583547ff824bcef3
Durgesh Tiwari <118502636+Durgesh2022@users.noreply.github.com>
2024-09-19 13:24:04 +0530

    Update layout.tsx

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
-  title: 'ZOOM CLONE',
+  title: 'Alcruit',
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
